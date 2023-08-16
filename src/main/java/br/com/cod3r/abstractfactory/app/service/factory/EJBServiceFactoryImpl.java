3e5441e84e32e0e7dfd9bf6439f04a0bac8a6c26
Leônidas Moritz Rosa <leogui.moritz@gmail.com>
2023-08-15 23:43:38 -0300

    Implementado a execução do padrão criacional abstract factory para criação de uma família de serviços de acordo com a tecnologia desejada EJB ou REST Api.

---
+++ src/main/java/br/com/cod3r/abstractfactory/app/service/factory/EJBServiceFactoryImpl.java
@@ -0,0 +1,18 @@
+package br.com.cod3r.abstractfactory.app.service.factory;
+
+import br.com.cod3r.abstractfactory.app.service.services.CarEJBService;
+import br.com.cod3r.abstractfactory.app.service.services.CarService;
+import br.com.cod3r.abstractfactory.app.service.services.UserEJBService;
+import br.com.cod3r.abstractfactory.app.service.services.UserService;
+
+public class EJBServiceFactoryImpl implements ServicesAbstractFactory {
+    @Override
+    public CarService createCarService() {
+        return new CarEJBService();
+    }
+
+    @Override
+    public UserService createUserService() {
+        return new UserEJBService();
+    }
+}
---
+++ src/main/java/br/com/cod3r/abstractfactory/app/service/factory/RestApiServiceFactoryImpl.java
@@ -0,0 +1,18 @@
+package br.com.cod3r.abstractfactory.app.service.factory;
+
+import br.com.cod3r.abstractfactory.app.service.services.CarRestApiService;
+import br.com.cod3r.abstractfactory.app.service.services.CarService;
+import br.com.cod3r.abstractfactory.app.service.services.UserRestApiService;
+import br.com.cod3r.abstractfactory.app.service.services.UserService;
+
+public class RestApiServiceFactoryImpl implements ServicesAbstractFactory {
+    @Override
+    public CarService createCarService() {
+        return new CarRestApiService();
+    }
+
+    @Override
+    public UserService createUserService() {
+        return new UserRestApiService();
+    }
+}
